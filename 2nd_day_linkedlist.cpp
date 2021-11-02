7fa8a56574840e3eea20d1627cbe2c6a309baa09
Ashutosh Jha <73036341+jhaashutosh@users.noreply.github.com>
2021-11-02 23:41:22 +0530

    Add files via upload

---
+++ 2nd_day_linkedlist.cpp
@@ -0,0 +1,20 @@
+// Linkedlist is majorly used when we are not known to the fact that how much data will be entered and is local memory allowing such big data for processing.
+// I think we all agree to the fact that array is much more easier to code and understand as compared to linkedlist and you all must know that there are few things
+// that are implemented just for programmer's ease of coding, for example stl library and recurssion, all these can be implemented directly but to reduce the time of
+// of coding we are using these direct methods.
+
+// Linkedlist has a lot of usage and we can modify it a little bit to make it much more effective.
+
+// code for implementation of linkedlist
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+}
+
+int main()
+{
+
+}
